7e46150b3c085c681d15fab19afa6f09d86bf440
ThomasFeldman <tfeldman1.31@gmail.com>
2020-05-14 00:58:57 -0500

    Starting app

---
+++ src/main/java/com/secretweapon/gtdNote/GtdNoteApplication.java
@@ -0,0 +1,13 @@
+package com.secretweapon.gtdNote;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GtdNoteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GtdNoteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/secretweapon/gtdNote/GtdNoteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.secretweapon.gtdNote;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GtdNoteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
